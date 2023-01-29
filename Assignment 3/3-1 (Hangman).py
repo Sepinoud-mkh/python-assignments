50d7bc99a3a4449975484d8e8bd62bef0fa479c4
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-01-29 20:32:57 +0330

    assignments

--- Assignment 3/3-1 (Hangman).py
+++ Assignment 3/3-1 (Hangman).py
@@ -2,6 +2,7 @@ import random
 
 words=["music","book","movie","laptop","telegram","exam"]
 word=random.choice(words).lower()
+true_Guess=[]
 guess=[]
 error=0
 
@@ -9,18 +10,17 @@ print("The word has %d letters"%len(word))
 for i in range(len(word)):
     print("-",end=" ")
 
-while (len(guess)!=len(word)) or (error<6):
-    for i in range(len(word)):
-        guess_char=input("\nPlease enter your guess:")
-        if guess_char in word:
-            guess.append(guess_char) 
-            print("The word has the letter that you guessed:",guess_char)
-        else:
-            guess.append("-")
-            error=error+1
-            print("The word doesn't have the letter that you guessed:" "-")
-if guess==word:
+while(len(true_Guess)!=len(word) and error<6):
+    guess_char=input("\nPlease enter your guess:")
+    if guess_char in word:
+        true_Guess.append(guess_char) 
+        print("The word has the letter that you guessed:",guess_char)
+    else:
+        guess.append("-")
+        error=error+1
+        print("The word doesn't have the letter that you guessed:" "-")
+if len(true_Guess)==len(word):
     print("You won!!")
-else:
+elif(error==6):
     print("You lost!")
     print(word)
