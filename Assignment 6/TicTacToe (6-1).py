7ab32199888f895aef98c8f924d2530265e1ace3
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-03-22 22:19:31 +0430

    nothing has changed

--- Assignment 6/TicTacToe (6-1).py
+++ Assignment 6/TicTacToe (6-1).py
@@ -27,7 +27,7 @@ def check_game():
         for j in range(3):
             if game_board[i][j]=="-":
                 num=num+1
-    if num<=3:
+    if num==0:
         if not(game_board[i][0]==game_board[i][1]==game_board[i][2]=="X" or game_board[0][i]==game_board[1][i]==game_board[2][i]=="X" or game_board[0][0]==game_board[1][1]==game_board[2][2]=="X" or game_board[0][2]==game_board[1][1]==game_board[2][0]=="X" or game_board[i][0]==game_board[i][1]==game_board[i][2]=="O" or game_board[0][i]==game_board[1][i]==game_board[2][i]=="O" or game_board[0][0]==game_board[1][1]==game_board[2][2]=="O" or game_board[0][2]==game_board[1][1]==game_board[2][0]=="O"):
             print("no one wins")
             end=time.time()
