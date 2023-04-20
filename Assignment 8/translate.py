f21e2c04e3c6cc4f7e947444fe9860e0cc7a20a4
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-04-21 00:08:26 +0430

    Assignments

--- Assignment 8/translate.py
+++ Assignment 8/translate.py
@@ -1,3 +1,5 @@
+from gtts import gTTS
+import os
 
 def readFromDatabase():
     global dictWords
@@ -20,41 +22,60 @@ def showMenu():
     print("3-Add new words")
     print("4-Exit")
 
-def writeToDatabase():
-    userNewWordEN=input("Enter the word in english:")
-    userNewWordPer=input("Enter the word in persian:")
-    newDict={"English":userNewWordEN,"Persian":userNewWordPer}
-    dictWords.append(newDict)
+def addNewWord():
+    newEngWord = input("Enter your english word to be added to the dictionary: ").lower()
+    
+    for word in dictWords:
+        if newEngWord == word["English"]:
+            print("your word is already exists")
+            print(word["English"], ":", word["Persian"] )
+            break
+    else:
+        newPerWord = input("Enter the persian meaning of your entered word: ").lower()
+        newWord = {"English":newEngWord, "Persian":newPerWord}
+        dictWords.append(newWord)
+        f = open("translate.txt", "a")
+        f.write("\n")
+        f.write(newEngWord)
+        f.write("\n")
+        f.write(newPerWord)
+        f.close()
+        print("you successfully add", newEngWord, ":", newPerWord, "to the dictionary")
 
 def English_to_Persian():
     userText=input("please enter your english text:")
-    userWords=userText.split(" ")
-    translation=" "
-    for userWord in userWords:
-        for word in dictWords:
-            if userWord==word["English"]:
-                translation=translation+word["Persian"]+" "
-                break
-        else:
-            translation=translation+userWord+" "
+    userSentences=userText.split(".")
+    for userSentence in userSentences: 
+        userWords=userSentences.split(" ")
+        translation=" "
+        for userWord in userWords:
+            for word in dictWords:
+                if userWord==word["English"]:
+                    translation=translation+word["Persian"]+" "
+                    break
+            else:
+                translation=translation+userWord+" "
 
     print(translation)
 
 def Persian_to_English():
-    userText=input("please enter your english text:")
-    userWords=userText.split(" ")
-    translation=" "
-    for userWord in userWords:
-        for word in dictWords:
-            if userWord==word["English"]:
-                translation=translation+word["Persian"]+" "
-                break
-        else:
-            translation=translation+userWord+" "
+    userText=input("please enter your persian text:")
+    userSentences=userText.split(".")
+    for userSentence in userSentences: 
+        userWords=userSentences.split(" ")
+        translation=" "
+        for userWord in userWords:
+            for word in dictWords:
+                if userWord==word["Persian"]:
+                    translation=translation+word["English"]+" "
+                    break
+            else:
+                translation=translation+userWord+" "
 
     print(translation)
-
-
+    audio=gTTS(text=translation,lang="en",slow=False)
+    audio.save("audio.mp3")
+    os.system("start audio.mp3")
 
 readFromDatabase()
 print("Welcome to the translator")
@@ -67,6 +88,6 @@ while True:
     elif choice==2:
         Persian_to_English()
     elif choice==3:
-        writeToDatabase()
+        addNewWord()
     elif choice==4:
         exit(0)
