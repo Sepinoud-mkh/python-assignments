b48e84c1a4624165b234b6b7fcb9035ce6c43b15
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-07-01 23:51:35 +0430

    Assignment

--- Assignment 26/26-1/26-1 (chess board).py
+++ Assignment 26/26-1/26-1 (chess board).py
@@ -9,39 +9,89 @@ column = C*60
 
 Board = np.zeros((row, column))
 
-#سطر فرد
+#1
+for a in range(60):
+    for k in range(0, 60):
+        Board[a][k] = 255
+    for k in range(120,180):
+        Board[a][k] = 255
+    for k in range(240,300):
+        Board[a][k] = 255
+    for k in range(360,420):
+        Board[a][k] = 255
+#2
+for a in range(60, 120):
+    for k in range(60, 120):
+        Board[a][k] = 255
+    for k in range(180, 240):
+        Board[a][k] = 255
+    for k in range(300, 360):
+        Board[a][k] = 255
+    for k in range(420, 480):
+        Board[a][k] = 255       
+#3
+for a in range(120, 180):
+    for k in range(60):
+        Board[a][k] = 255
+    for k in range(120,180):
+        Board[a][k] = 255
+    for k in range(240,300):
+        Board[a][k] = 255
+    for k in range(360,420):
+        Board[a][k] = 255
+#4
+for a in range(180, 240):
+    for k in range(60, 120):
+        Board[a][k] = 255
+    for k in range(180, 240):
+        Board[a][k] = 255
+    for k in range(300, 360):
+        Board[a][k] = 255
+    for k in range(420, 480):
+        Board[a][k] = 255 
+#5
+for a in range(240, 300):
+    for k in range(60):
+        Board[a][k] = 255
+    for k in range(120,180):
+        Board[a][k] = 255
+    for k in range(240,300):
+        Board[a][k] = 255
+    for k in range(360,420):
+        Board[a][k] = 255
 
-for i in range(1, R+1):
-    for a in range(60*2*i, 60*(2*i+1)):
-        for k in range(60*2*i, 60*(2*i+1)):
-            Board[a][k] = 255
-#for k in range(120,180):
-#    Board[a][k] = 255
-#for k in range(240,300):
-#    Board[a][k] = 255
-#for k in range(360,420):
-#    Board[a][k] = 255
-    #سطر زوج
-#for a in range(60):
-#    for k in range(60, 120):
-#        Board[a][k] = 255
-#    for k in range(180, 240):
-#        Board[a][k] = 255
-#    for k in range(300, 360):
-#        Board[a][k] = 255
-#    for k in range(420, 480):
-#        Board[a][k] = 255
+#6
+for a in range(300, 360):
+    for k in range(60, 120):
+        Board[a][k] = 255
+    for k in range(180, 240):
+        Board[a][k] = 255
+    for k in range(300, 360):
+        Board[a][k] = 255
+    for k in range(420, 480):
+        Board[a][k] = 255 
+#7
+for a in range(360, 420):
+    for k in range(60):
+        Board[a][k] = 255
+    for k in range(120,180):
+        Board[a][k] = 255
+    for k in range(240,300):
+        Board[a][k] = 255
+    for k in range(360,420):
+        Board[a][k] = 255      
+#8
+for a in range(420, 480):
+    for k in range(60, 120):
+        Board[a][k] = 255
+    for k in range(180, 240):
+        Board[a][k] = 255
+    for k in range(300, 360):
+        Board[a][k] = 255
+    for k in range(420, 480):
+        Board[a][k] = 255 
 
-#for a in range(120, 180):
-#    for k in range(60):
-#        Board[a][k] = 255
-#    for k in range(120,180):
-#        Board[a][k] = 255
-#    for k in range(240,300):
-#        Board[a][k] = 255
-#    for k in range(360,420):
-#        Board[a][k] = 255
-#
+
 
 
 cv2.imshow("",Board)
