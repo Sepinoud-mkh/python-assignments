5d08fb4411a0830c75d62827cbe2598fcc95a961
Taha Farshbaf <t_farshbaf@outlook.com>
2023-07-02 13:46:03 +0330

    Update 26-1 (chess board).py

--- Assignment 26/26-1/26-1 (chess board).py
+++ Assignment 26/26-1/26-1 (chess board).py
@@ -1,96 +1,18 @@
 import numpy as np
 import cv2
 
+board_size = 480
+board = np.zeros((board_size, board_size))
 
-Board = np.zeros((480, 480))
+squares_per_side = 8
+square_size = board_size // squares_per_side
 
+for row in range(squares_per_side):
+    for col in range(squares_per_side):
+        if (row + col) % 2 == 0:
+            board[row * square_size: (row + 1) * square_size, col * square_size: (col + 1) * square_size] = 255
 
-#1
-for a in range(0, 60):
-    for k in range(60):
-        Board[a][k] = 255
-    for k in range(120,180):
-        Board[a][k] = 255
-    for k in range(240,300):
-        Board[a][k] = 255
-    for k in range(360,420):
-        Board[a][k] = 255
-#2
-for a in range(60, 120):
-    for k in range(60, 120):
-        Board[a][k] = 255
-    for k in range(180, 240):
-        Board[a][k] = 255
-    for k in range(300, 360):
-        Board[a][k] = 255
-    for k in range(420, 480):
-        Board[a][k] = 255       
-#3
-for a in range(120, 180):
-    for k in range(60):
-        Board[a][k] = 255
-    for k in range(120,180):
-        Board[a][k] = 255
-    for k in range(240,300):
-        Board[a][k] = 255
-    for k in range(360,420):
-        Board[a][k] = 255
-#4
-for a in range(180, 240):
-    for k in range(60, 120):
-        Board[a][k] = 255
-    for k in range(180, 240):
-        Board[a][k] = 255
-    for k in range(300, 360):
-        Board[a][k] = 255
-    for k in range(420, 480):
-        Board[a][k] = 255 
-#5
-for a in range(240, 300):
-    for k in range(60):
-        Board[a][k] = 255
-    for k in range(120,180):
-        Board[a][k] = 255
-    for k in range(240,300):
-        Board[a][k] = 255
-    for k in range(360,420):
-        Board[a][k] = 255
-
-#6
-for a in range(300, 360):
-    for k in range(60, 120):
-        Board[a][k] = 255
-    for k in range(180, 240):
-        Board[a][k] = 255
-    for k in range(300, 360):
-        Board[a][k] = 255
-    for k in range(420, 480):
-        Board[a][k] = 255 
-#7
-for a in range(360, 420):
-    for k in range(60):
-        Board[a][k] = 255
-    for k in range(120,180):
-        Board[a][k] = 255
-    for k in range(240,300):
-        Board[a][k] = 255
-    for k in range(360,420):
-        Board[a][k] = 255      
-#8
-for a in range(420, 480):
-    for k in range(60, 120):
-        Board[a][k] = 255
-    for k in range(180, 240):
-        Board[a][k] = 255
-    for k in range(300, 360):
-        Board[a][k] = 255
-    for k in range(420, 480):
-        Board[a][k] = 255 
-
-
-
-
-cv2.imshow("",Board)
-cv2.imwrite("26-1\Chess Board.jpg", Board)
+cv2.imshow("", board)
+cv2.imwrite("26-1\Chess Board.jpg", board)
 
 cv2.waitKey()
