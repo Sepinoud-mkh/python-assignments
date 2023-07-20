c5a9203aa1a282dd6a1b47376c6710e1856c7b6c
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-07-20 20:50:17 +0430

    Assignment

--- Assignment 26/26-4/26-4.py
+++ Assignment 26/26-4/26-4.py
@@ -2,33 +2,21 @@ import numpy as np
 import cv2
 
 
-board = np.ones((480, 480)) * 255
+board = np.ones((480, 480))*255
+
+board[100:160, 300:360] = 0
+
+board[40:100, 200:300] = 0
+board[200:260, 200:300] = 0
+board[340:400, 200:300] = 0
+
+board[100:200, 140:200] = 0
+board[260:340, 300:360] = 0
+
+board[280:340, 140:200] = 0
 
 
-#for i in range(0, 480):
-#    for j in range(0, 100):
-#        board[i][j] = 255
-#    for j in range(380, 480):
-#        board[i][j] = 255
 
-#for i in range(0, 50):
-#    for j in range(100, 380):
-#        board[i][j] = 255
-
-#for i in range(360, 480):
-#    for j in range(120, 360):
-#        board[i][j] = 255
-
-#for j in range(100, 380):
-#    for i in range(0, 50):
-#        board[i][j] = 255
-#    for i in range(430, 480):
-#        board[i][j] = 255
-
-for j in range(180, 300):
-    for i in range(100, 160):
-        board[i][j] = 0
-for j in range(1kk)
 cv2.imshow("", board)
 cv2.imwrite("name.jpg", board)
 cv2.waitKey()
