cf96ef5c82b6efd393ac1c6c3e2b3dceb2fe5d1f
Sepinoud-mkh <sepinoud.marhamati@gmail.com>
2023-08-27 12:57:37 +0430

    image processing

--- Assignment 27/27-3/27-3.py
+++ Assignment 27/27-3/27-3.py
@@ -4,29 +4,45 @@ import random
 
 image = cv2.imread("lonely-japanese-cherry.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.resize(image, (600, 400))
+image = cv2.resize(image, (400, 600))
 
-print(image.shape)
-# for row in range(0, 600, 50):
-#     for col in range(0, 400, 20):
-#         cv2.circle(image, (row, col), 1, 200, 2)
+rows, cols = image.shape
 
-# cv2.imshow("result", image)
-# cv2.waitKey()
+#writer = cv2.VideoWriter('snow.mp4', cv2.VideoWriter_fourcc(*'mpv4'), 10, (cols, rows))
+
+
+t = image[399, 599]
+print(t)
+
+
 
 while True:
 
+    r = 0
+    row = random.randint(0, 400)
+    col = random.randint(0, 600)
+   
 
-    col = 0
-    while col != 600:
+    while r != row:
 
-        row = random.randint(0, 400)
-        cv2.circle(image, (col, row), 1, 0, 2)
-        col = col + 10
+        temp = image[r, col]
+        temp =  int(temp)
+        cv2.circle(image, (col, r), 1, 0, 2)
+        cv2.circle(image, (col, r + 10), 1, 100, 2)
 
-    cv2.imshow("result", image)
+        cv2.waitKey(15)
+        cv2.circle(image, (col, r), 1, temp, 2)
+        if r <= 390:
+            r = r + 10
+        else:
+            row = random.randint(0, 400)
+            print("hi")
+
+
+        cv2.imshow("result", image)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
 
+#writer.release()
